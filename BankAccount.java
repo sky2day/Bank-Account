a220704379c318d62894689d67c700ba7091aefe
Andrei Stoma <Andrei Stoma>
2016-09-26 17:30:23 -0700

    Andrei's changes

--- BankAccount.java
+++ BankAccount.java
@@ -1,117 +1,107 @@
-package bankaccount;
-// BankAccount:
-// Prompts the user to input their name, ID, and account balance
-// Creates a BankAccount class to store values
-// Prompts user to enter 1,2,3,4,5 to either
-// 1- check the balance
-// 2- deposit money
-// 3- withdraw money
-// 4- adds interest
-// 5- exits program (or any other integer)
-// Created by Van Ralf Aguas and Tyler Hoh
-// CS 11B Jessica Masters
-// September 28, 2016
+package ccsf111b;
 
 public class BankAccount {
-    
-    private String accountOwner;
-    private String accountID;
-    private double accountBalance;
-    private double accountInterest;
-    
-    public BankAccount(String owner, String ID, double balance, double interest){ //constructor
-        accountOwner = owner;
-        accountID = ID;
-        accountBalance = balance;
-        accountInterest = interest;
-    }
-    
-    // gets account owner
-    public String getAccountOwner(){ 
-        return accountOwner;
-    }
-    // gets account ID
-    public String getAccountID(){ 
-        return accountID;
-    }
-    // gets account balance
-    public double getAccountBalance(){ 
-        return accountBalance;
-    }
-    // gets account interest
-    public double getAccountInterest(){ 
-        return accountInterest;
-    }
-    // sets account owner
-    public void setAccountOwner(String owner){ 
-        if(owner.length() == 0){
-                System.out.println("You must have a name! Please enter your name.");
-        }
-        else{
-            accountOwner = owner;
-        }
-    }
-    // sets account ID
-    public void setAccountID(String ID){ 
-        if(ID.length() == 0){
-            System.out.println("You must have an ID! Please enter your ID.");
-        }
-        else{
-            accountID = ID;
-        }
-    }
-    // sets account balance
-    public void setAccountBalance(double balance){ 
-        if(balance == 0){
-                System.out.println("You must have a starting balance! Please enter your account balance.");
-        }
-        else if(balance < 0){
-                System.out.println("You cannot have a negative starting balance! Please enter a positive account balance.");
-            }
-        else{
-            accountBalance = balance;
-        }
-    }
-    // sets account interest
-    public void setAccountInterest(double interest){ 
-        accountInterest = interest/100; //to turn into a decimal value
-    }
-    // method to deposit amount
-    public void deposit(double depositAmount){
-        if(depositAmount == 0){
-            System.out.println("No money was deposited in this transaction.");
-        }
-        else if(depositAmount < 0.01)
-            System.out.println("Please enter a larger amount");
-        else{
-            accountBalance += depositAmount;
-            setAccountBalance(accountBalance);
-        }
-    }
-    // method to withdraw amount
-    public void withdraw(double withdrawAmount){
-        if(withdrawAmount > accountBalance)
-            System.out.println("Insufficient funds. Please enter a valid amount.");
-        else if(withdrawAmount == 0){
-            System.out.println("No money was withdrawn from this transaction");
-        }
-        else if(withdrawAmount < 0){
-            System.out.println("Please enter a positive, non zero amount.");
-        }
-        else{
-            accountBalance -= withdrawAmount;
-            setAccountBalance(accountBalance);
-        }
-    }
-    //adds interest to balance
-    public void addInterest(){
-        setAccountBalance(accountBalance += (accountBalance * accountInterest));
-    }
-    //toString method
-    public String toString(){
-        return "Username: " + accountOwner 
-              +"\nID: "       + accountID
-              +"\nInterest Rate: " + (accountInterest*100) + "%"
-              +"\nBalance: "  + accountBalance;
-    }
+
+	private String accountOwner;
+	private String accountID;
+	private double accountBalance;
+	private double accountInterest;
+
+	public BankAccount(String owner, String ID, double balance, double interest) { // constructor
+		accountOwner = owner;
+		accountID = ID;
+		accountBalance = balance;
+		accountInterest = interest/100;
+	}
+
+	// gets account owner
+	public String getAccountOwner() {
+		return accountOwner;
+	}
+
+	// gets account ID
+	public String getAccountID() {
+		return accountID;
+	}
+
+	// gets account balance
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+
+	// gets account interest
+	public double getAccountInterest() {
+		return accountInterest;
+	}
+
+	// sets account owner
+	public void setAccountOwner(String owner) {
+		if (owner.length() == 0) {
+			System.out.println("You must have a name! Please enter your name.");
+		} else {
+			accountOwner = owner;
+		}
+	}
+
+	// sets account ID
+	public void setAccountID(String ID) {
+		if (ID.length() == 0) {
+			System.out.println("You must have an ID! Please enter your ID.");
+		} else {
+			accountID = ID;
+		}
+	}
+
+	// sets account balance
+	public void setAccountBalance(double balance) {
+		if (balance == 0) {
+			System.out.println("You must have a starting balance! Please enter your account balance.");
+		} else if (balance < 0) {
+			System.out.println("You cannot have a negative starting balance! Please enter a positive account balance.");
+		} else {
+			accountBalance = balance;
+		}
+	}
+
+	// sets account interest
+	public void setAccountInterest(double interest) {
+		accountInterest = interest / 100; // to turn into a decimal value
+	}
+
+	// method to deposit amount
+	public void deposit(double depositAmount) {
+		if (depositAmount == 0) {
+			System.out.println("No money was deposited in this transaction.");
+		} else if (depositAmount < 0.01)
+			System.out.println("Please enter a larger amount");
+		else {
+			accountBalance += depositAmount;
+			setAccountBalance(accountBalance);
+		}
+	}
+
+	// method to withdraw amount
+	public void withdraw(double withdrawAmount) {
+		if (withdrawAmount > accountBalance)
+			System.out.println("Insufficient funds. Please enter a valid amount.");
+		else if (withdrawAmount == 0) {
+			System.out.println("No money was withdrawn from this transaction");
+		} else if (withdrawAmount < 0) {
+			System.out.println("Please enter a positive, non zero amount.");
+		} else {
+			setAccountBalance(accountBalance -= withdrawAmount);
+		}
+	}
+
+	// adds interest to balance
+	public void addInterest() {
+		setAccountBalance(accountBalance += (accountBalance * accountInterest));
+	}
+
+	// toString method
+	public String toString() {
+		return "Username: " + accountOwner + "\nID: " + accountID
+				+ "\nInterest Rate: " + (accountInterest * 100) + "%"
+				+ "\nBalance: " + accountBalance;
+	}
 }
--- BankAccountDriver.java
+++ BankAccountDriver.java
@@ -1,99 +1,106 @@
-// BankAccountDriver:
-// Prompts the user to input their name, ID, and account balance
-// Creates a BankAccount class to store values
-// Prompts user to enter 1,2,3,4,5 to either
-// 1- check the balance
-// 2- deposit money
-// 3- withdraw money
-// 4- adds interest
-// 5- exits program
-// Created by Van Ralf Aguas and Tyler Hoh
-// CS 11B Jessica Masters
-// September 28, 2016
+package ccsf111b;
 
-import bankaccount.BankAccount;
+//import bankaccount.BankAccount; - don't have to import your own class
 import java.util.Scanner;
 
 public class BankAccountDriver {
-        public static void main(String[] args) {
-        
-        String username;
-        String userID;
-        double userBalance;
-        double userInterest;
-        double amount; // amount to withdraw or deposit
-        int choice; // variable to determine user action
-        
-        Scanner userInput = new Scanner(System.in);
-        System.out.println("Welcome to the Bank Account!");
-        
-        System.out.println("What is your name?");
-        do{
-            username = userInput.nextLine();
-            if(username.length() == 0){
-                System.out.println("You must have a name! Please enter your name.");
-            }
-        }while(username.length() == 0);
-        
-        System.out.println("What is your account ID?");
-        do{
-            userID = userInput.nextLine();
-            if(userID.length() == 0){
-                System.out.println("You must have an ID! Please enter your ID.");
-            }
-        }while(userID.length() == 0);
+	public static void main(String[] args) {
 
-        System.out.println("What is your account balance?");
-        do{
-            userBalance = userInput.nextDouble();
-            if(userBalance == 0){
-                System.out.println("You must have a starting balance! Please enter your account balance.");
-            }
-            else if(userBalance < 0){
-                System.out.println("You cannot have a negative starting balance! Please enter a positive account balance.");
-            }
-        }while(userBalance == 0 || userBalance < 0);
- 
-        System.out.println("How much interest would you like?");
-        userInterest = userInput.nextDouble();
-        
-        BankAccount newUser = new BankAccount(username, userID, userBalance,userInterest);
-        newUser.setAccountInterest(userInterest);
+		String username;
+		String userID;
+		double userBalance;
+		double userInterest;
+		double amount; // amount to withdraw or deposit
+		int choice; // variable to determine user action
 
-        do{
-            System.out.println("Press 1 to check balance." + 
-                               "\nPress 2 to deposit money." +
-                               "\nPress 3 to withdraw money." +
-                               "\nPress 4 to add interest." +
-                               "\nPress 5 to exit program.");
-            
-            choice = userInput.nextInt();
-            
-            switch(choice){
-                case 1: 
-                    System.out.println("Your balance is: $" + newUser.getAccountBalance());
-                    break;
-                case 2:
-                    System.out.println("How much would you like to deposit?");
-                    amount = userInput.nextDouble();
-                    newUser.deposit(amount);
-                    amount = 0;
-                    break;           
-                case 3:
-                    System.out.println("How much would you like to withdraw?");
-                    amount = userInput.nextDouble();
-                    newUser.withdraw(amount);
-                    amount = 0;
-                    break;
-                case 4:
-                    newUser.addInterest();
-                    break;
-                default:
-                    amount = 0;
-                    break;
-            }
-        }while(choice < 5 && choice >= 0);
-        
-        System.out.println(newUser.toString());
-    }
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Welcome to the Bank Account!");
+
+		System.out.println("What is your name?");
+		do {
+			username = userInput.nextLine();
+			if (username.length() == 0) {
+				System.out
+						.println("You must have a name! Please enter your name.");
+			}
+		} while (username.length() == 0);
+
+		System.out.println("What is your account ID?");
+		do {
+			userID = userInput.nextLine();
+			if (userID.length() == 0) {
+				System.out
+						.println("You must have an ID! Please enter your ID.");
+			}
+		} while (userID.length() == 0);
+
+		System.out.println("What is your initial deposit?");
+		do {
+			userBalance = userInput.nextDouble();
+			//userBalance = Double.parseDouble(userInput.nextLine());
+			if (userBalance == 0) {
+				System.out
+						.println("You must have a starting balance! Please enter your initial deposit.");
+			} else if (userBalance < 0) {
+				System.out
+						.println("You cannot have a negative starting balance! Please enter a positive amount.");
+			}
+		} while (userBalance <= 0);
+
+		System.out.println("How much interest would you like?");
+		userInterest = userInput.nextDouble();
+
+		BankAccount newUser = new BankAccount(username, userID, userBalance,
+				userInterest);
+		// newUser.setAccountInterest(userInterest);
+		do {
+			do {
+				System.out.println("\nPlese select one of the following options: "
+								+ "\nPress 1 to check balance."
+								+ "\nPress 2 to deposit money."
+								+ "\nPress 3 to withdraw money."
+								+ "\nPress 4 to add interest."
+								+ "\nPress 5 to exit program.");
+
+				choice = userInput.nextInt();
+				if (choice > 5 || choice <= 0) {
+					System.out.print("Please enter a valid choice. ");
+				}
+			} while (choice > 5 || choice <= 0);
+
+			switch (choice) {
+			case 1:
+				System.out.println("Your balance is: $"
+						+ newUser.getAccountBalance());
+				break;
+			case 2:
+				do{
+				System.out.println("How much would you like to deposit?");
+				amount = userInput.nextDouble();
+				newUser.deposit(amount);}
+				while (amount < 0);
+				amount = 0;
+				break;
+			case 3:
+				do{
+				System.out.println("How much would you like to withdraw?");
+				amount = userInput.nextDouble();
+				newUser.withdraw(amount);}
+				while(amount <0 || amount> newUser.getAccountBalance());
+				
+				amount = 0;
+				break;
+			case 4:
+				newUser.addInterest();
+				break;
+			case 5:
+				System.out.println("Thank you for using your bank account. Program will quit now.");
+			default:
+				break;
+			}
+		}
+
+		while (choice != 5);
+		System.out.println(newUser.toString());
+	}
 }
